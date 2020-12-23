5eda7abcb1fdc1a1ba01f5113553021e470134cd
Stephen Paine <smpaine@gmail.com>
2020-12-23 14:05:40 -0500

    Changed JwtResponse object to better match User object

--- src/main/java/com/nameniap/mpgtracker/model/JwtResponse.java
+++ src/main/java/com/nameniap/mpgtracker/model/JwtResponse.java
@@ -7,27 +7,34 @@ public class JwtResponse implements Serializable {
 
 	private static final long serialVersionUID = -8091879091924046844L;
 	private final String token;
+	private final int id;
 	private final String userName;
-	private final Date lastLoginDate;
 	private final String userType;
+	private final Date lastLoginDt;
+	
 
 	public JwtResponse(String token, User user) {
 		this.token = token;
+		this.id = user.getId();
 		this.userName = user.getUserName();
-		this.lastLoginDate = user.getLastLoginDt();
+		this.lastLoginDt = user.getLastLoginDt();
 		this.userType = user.getUserType();
 	}
 
 	public String getToken() {
 		return this.token;
 	}
+	
+	public int getId() {
+		return id;
+	}
 
 	public String getUserName() {
 		return userName;
 	}
 
-	public Date getLastLoginDate() {
-		return lastLoginDate;
+	public Date getLastLoginDt() {
+		return lastLoginDt;
 	}
 
 	public String getUserType() {
