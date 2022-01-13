fa36b7bc131c7031fd8e6c86b0ef821a219aa91d
Stephen Paine <smpaine@gmail.com>
2022-01-13 14:40:35 -0500

    Added override of application initialization for future expansion, and moved packaging higher up in pom.xml (rearranged).

--- src/main/java/com/nameniap/mpgtracker/MPGTrackerApplication.java
+++ src/main/java/com/nameniap/mpgtracker/MPGTrackerApplication.java
@@ -2,6 +2,7 @@ package com.nameniap.mpgtracker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 /**
@@ -12,6 +13,11 @@ import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
  */
 @SpringBootApplication
 public class MPGTrackerApplication extends SpringBootServletInitializer {
+	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(MPGTrackerApplication.class);
+	}
 
     public static void main(String[] args) {
         SpringApplication.run(MPGTrackerApplication.class, args);
